69719d614acb18816955de16cb4ae4ca2c172045
Georgi Naumov <gonaumov@gmail.com>
2015-01-04 15:13:49 +0200

    Remove dependance of the jQuery library, separation of new names into a new file and a number of improvements.

---
+++ config.js
@@ -0,0 +1,30 @@
+/**
+ * Here I separate the real names so they can be managed more easily.
+ * @type {{name: string, realName: string}[]}
+ */
+var realNames = [
+    {
+        name: "Бареков",
+        realName: "Бюреков"
+    },
+    {
+        name: "БСП",
+        realName: "БКП"
+    },
+    {
+        name: "Орешарски",
+        realName: "Олигархски"
+    },
+    {
+        name: "Станишев",
+        realName: "Съселов"
+    },
+    {
+        name: "Волен",
+        realName: "Болен"
+    },
+    {
+        name: "Атака",
+        realName: "Юруш"
+    }
+];
--- rename.js
+++ rename.js
@@ -1,8 +1,31 @@
-(function($) {
-    
-    $(function(){
-    var thePage = $("body");
-    thePage.html(thePage.html().replace(/Бареков/g, 'Бюреков').replace(/БСП/g, 'БКП').replace(/Орешарски/g, 'Олигархски').replace(/Станишев/g, 'Съселов').replace(/Волен/g, 'Болен').replace(/Атака/g, 'Юруш'));
-    });
- 
-})(jQuery)
+(function () {
+    /**
+     * We do not need scripts, styles
+     * and links.
+     */
+    var selector = ":not(script):not(style):not(link)";
+    var pageNodes = Array.prototype.slice.call(document.querySelectorAll(selector));
+
+    if (pageNodes.length == 0) {
+        return;
+    }
+
+    /**
+     * The idea here is to make replacement
+     * asynchronously.
+     */
+    (function replaceNames(domNode) {
+
+        realNames.forEach(function (item) {
+            var re = new RegExp(item.name, "gi");
+            domNode.innerHTML = domNode.innerHTML.replace(re, item.realName);
+        });
+
+        if (pageNodes.length != 0) {
+            setTimeout(function () {
+                replaceNames(pageNodes.shift());
+            }, 0);
+        }
+    }(pageNodes.shift()));
+
+}());
